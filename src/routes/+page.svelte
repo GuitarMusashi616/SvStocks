7d9b21b6f237214f56bcb5d9a8adb2f4c8d46856
Austin Williams <awilliams@Austins-MacBook-Pro.local>
2025-07-15 20:04:04 -0800

    Configure pages for prerendering

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+    export const prerender = true;
+    let count = $state(0);
+    function inc_count() {
+        count += 1
+    }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button onclick={inc_count}>
+    <p>Click {count}</p>
+</button>
